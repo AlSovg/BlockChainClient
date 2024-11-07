d444b4eee476f6f6acc740a8c606b4ad2c5395c9
AlSovg <141185449+AlSovg@users.noreply.github.com>
2024-11-07 23:59:43 +0300

    Миграция

--- BlockChain.Domain/Migrations/20241107204204_Blocks_table.Designer.cs
+++ BlockChain.Domain/Migrations/20241107204204_Blocks_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlockChain.Domain.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlockChain.Domain.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241107204204_Blocks_table")]
+    partial class Blocks_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BlockChain.Domain/Migrations/20241107204204_Blocks_table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlockChain.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Blocks_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blocks",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    block_id = table.Column<int>(type: "integer", nullable: false),
+                    cur_hash = table.Column<string>(type: "text", nullable: false),
+                    prev_hash = table.Column<string>(type: "text", nullable: false),
+                    is_genesis = table.Column<bool>(type: "boolean", nullable: false),
+                    valid_count = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blocks", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blocks");
+        }
+    }
+}
